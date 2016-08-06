13e7b33dd9dda9c8250d39041e3967efc312a9f2
David Mugnai <dvd@gnx.it>
2016-08-06 13:46:55 +0200

    recipe rewritten to support compiled library

--- conanfile.py
+++ conanfile.py
@@ -7,18 +7,28 @@ class FmtConan(ConanFile):
     version = "3.0.0"
     license = "BSD"
     url = "https://github.com/memsharded/conan-fmt"
-    build_policy = "missing"
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = "shared=False", "fPIC=True"
 
     def source(self):
        self.run("git clone https://github.com/fmtlib/fmt")
        self.run("cd fmt && git checkout 3.0.0")
 
+    def build(self):
+       shared = "-DBUILD_SHARED_LIBS=ON" if self.options.shared else ""
+       fpic = "-DCMAKE_POSITION_INDEPENDENT_CODE=TRUE" if self.options.fPIC else ""
+       cmake = CMake(self.settings)
+       self.run("cd fmt && cmake . %s %s %s" % (cmake.command_line, shared, fpic))
+       self.run("cd fmt && cmake --build . %s" % cmake.build_config)
+
     def package(self):
         self.copy("*.h", dst="include/fmt", src="fmt/fmt")
-        self.copy("*.h", dst="include/cppformat", src="cppformat")
-        self.copy("*.cc", dst="include/fmt", src="fmt/fmt")
-        self.copy("*.h", dst="include/cppformat", src="cppformat")
+        self.copy("*.a", dst="lib", src="fmt/fmt", keep_path=False)
+        self.copy("*.so*", dst="lib", src="fmt/fmt", keep_path=False)
+        self.copy("*.lib", dst="lib", src="fmt/fmt", keep_path=False)
+        self.copy("*.dll", dst="bin", src="fmt/fmt", keep_path=False)
 
     def package_info(self):
-        self.cpp_info.defines.append("FMT_HEADER_ONLY")
+        self.cpp_info.libs = ["fmt"]
 
